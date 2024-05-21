d8c5a7ee2fa5e3d9160ecc9e7388c76ce4064c57
Cyras117 <alexsandro.oliveira117@gmail.com>
2024-05-21 18:46:18 -0300

    ch1-lessions

---
+++ ch1_Classes/challenges/LIBRARY/main.py
@@ -0,0 +1,24 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+class Library:
+    def __init__(self, name):
+        self.name = name
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    def remove_book(self, book):
+        for b in self.books:
+            if b.title == book.title and b.author == book.author:
+                self.books.remove(b)
+
+    def search_books(self, search_string):
+        search_list = []
+        for b in self.books:
+            if search_string.lower() in b.title.lower() or search_string.lower() in b.author.lower():
+                search_list.append(b)
+        return search_list
---
+++ ch1_Classes/lessions/8_METHODS/main.py
@@ -0,0 +1,5 @@
+class Wall:
+    armor = 10
+    height = 5
+    def fortify(self):
+        self.armor += self.armor
